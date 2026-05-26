bae53100020821905e6b68ae9a43c545fc5553ad
Superfang0726 <yaya072610@gmail.com>
2026-05-26 16:06:16 +0800

    For syncing progress to other computor

--- lib/chat/gemini_repository.dart
+++ lib/chat/gemini_repository.dart
@@ -1,6 +1,23 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+
 class GeminiRepository {
   //constructor
-  GeminiRepository();
+  String apiKey;
+  String modelName;
 
+  GeminiRepository({
+    required this.apiKey,
+    required this.modelName,
+  }); //(No need params when implementation)
+
+  GenerativeModel model = GenerativeModel(model: modelName, apiKey: apiKey);
   //method
+}
+
+void main() async {
+  final apiKey = 'AIzaSyDYPZi5gXCgRnVju9kMC2s5atwXMpOaLpE';
+  final modelName = 'gemma-4-31b-it';
+  final prompt = 'Say Hello';
+
+  GeminiRepository geminiRepository = GeminiRepository();
 }
